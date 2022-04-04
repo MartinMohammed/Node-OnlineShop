6801ab3b1ae1d5249af9f9587f412cb640575f6b
Martin Mohammed <81469658+MartinMohammed@users.noreply.github.com>
2022-04-04 21:25:56 +0200

    mongodb native driver implementation of shop system finsih; start mongoose framework

--- controllers/MongoDB/shop.js
+++ controllers/MongoDB/shop.js
@@ -1,5 +1,6 @@
 // * ---------------------------- USING MONGODB ------------------------
 const Product = require("../../models/MongoDB/product");
+const User = require("../../models/MongoDB/user");
 // const Cart = require("../../models/MongoDB/cart")
 
 exports.getHomepage = (req, res, next) => {
@@ -40,16 +41,64 @@ exports.getProduct = (req, res, next) => {
   });
 };
 
+exports.getCart = (req, res, next) => {
+  // return the cart of the current user
+  req.user
+    .getCart()
+    .then((products) => {
+      res.render("shop/cart", {
+        pageTitle: "Cart",
+        path: "/cart",
+        products,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
 exports.postCart = (req, res, next) => {
+  // prodId from hidden input field
   const prodId = req.body.productId;
   Product.findById(prodId)
     .then((product) => {
-      // * Now user is an instance of the User class (access to its static / normal methods)
+      // * Now user (app.js) is an instance of the User class (access to its static / normal methods)
       // return another promise
       return req.user.addToCart(product);
     })
     .then((result) => {
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
+
+exports.postCartDeleteItem = (req, res, next) => {
+  const productId = req.body.productId;
+  req.user
+    .deleteItemFromCart(productId)
+    .then((result) => {
       console.log(result);
+      res.redirect("/cart");
+    })
+    .catch((err) => console.log(err));
+};
+exports.getOrders = (req, res, next) => {
+  req.user
+    .getOrders()
+    .then((orders) => {
+      console.log(orders);
+      res.render("shop/orders", {
+        path: "/orders",
+        pageTitle: "Your Orders",
+        orders: orders,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+exports.postOrder = (req, res, next) => {
+  req.user
+    .addOrder()
+    .then((result) => {
+      console.log(result, "successfully placed the order");
+      res.redirect("/orders");
     })
     .catch((err) => console.log(err));
 };
--- models/MongoDB/user.js
+++ models/MongoDB/user.js
@@ -1,3 +1,4 @@
+const res = require("express/lib/response");
 const mongodb = require("mongodb");
 const getDb = require("../../util/database").getDb;
 
@@ -6,7 +7,11 @@ class User {
   constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
-    this.cart = cart; // {items: []}
+    // fetched from app.js User.findById
+    this.cart = cart; // {items: [{productId Type: ObjectId, quantity}]}
+
+    // dont confuse this._id with productId of a cartItem
+    // type ObjectId accepted for mongodb
     this._id = id;
   }
 
@@ -17,14 +22,34 @@ class User {
 
   addToCart(product) {
     // // if this returns something which is not -1 then the product exist already in the cart
-    // const cartProduct = this.cart.items.findIndex((cp) => {
-    //   return (cp._id = product._id);
-    // });
-    // if (cartProduct === -1){
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      // ! product is a instace retrieved from the database, we can treat ObjectId as string
+      // but its type is not String
+      // iterate through every cartProduct until the function returns true;
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
 
-    // }
-    // ---------------- IF PRODUCT IS NOT IN THE CART -----------
-    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    // if item already exists
+    if (cartProductIndex >= 0) {
+      // get the product in the user cart and increase the quantity
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      // ---------------- IF PRODUCT IS NOT IN THE CART -----------
+      // No duplicate data instead a reference to the product
+      // *  items will be a property of a user instance / save reference to the product
+      // avoid data duplication
+      updatedCartItems.push({
+        productId: new mongodb.ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
+    const updatedCart = {
+      items: updatedCartItems,
+    };
+
     const db = getDb();
     return db.collection("users").updateOne(
       // update the users cart
@@ -33,6 +58,64 @@ class User {
       { $set: { cart: updatedCart } }
     );
   }
+
+  // * return cart of the current user
+  getCart() {
+    // Because of Mongo relations one-to-one user & cart
+    // we can simply return the cart
+
+    // * but if would be great when we return a fully polulated cart, so a cart
+    // * with all the product details which we also require.
+    // return this.cart;
+
+    const db = getDb();
+    // * find all products that are in the cart / use some special mongdb query operators
+    // * $in operator takes an array of IDs : every id which is in the array will be accepted and
+    // * will get back a cursor which holds references to all products with one of the id's mentioned
+    // * in the array
+    //   ---------------- MERGE DATA MANUALLY -----------------
+    const productIds = this.cart.items.map((i) => i.productId);
+    return db
+      .collection("products")
+      .find({ _id: { $in: [...productIds] } })
+      .toArray()
+      .then((products) => {
+        // add the quantity back to the products
+        // * this returns an array of [{product, ItsQuantity from UserCart}]
+        return products.map((p) => {
+          // *  first find th cartItem(userItem) where its productId (as a reference)
+          // * machtes the _id of a Product
+          // * second give us the quantity of the given product as cartItem
+          const quantity = this.cart.items.find((cartItem) => {
+            return p._id.toString() === cartItem.productId.toString();
+          }).quantity;
+          return {
+            ...p,
+            quantity,
+          };
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  deleteItemFromCart(productId) {
+    const db = getDb();
+    // 1. fetch all cartItems from the current User (stored this)
+    // 2. delete the cartItem we dont want from 1.
+    // 3. update cartItems of the current User
+    const updatedCartItems = this.cart.items.filter(
+      (cartItem) => cartItem.productId.toString() !== productId.toString()
+    );
+    return db
+      .collection("users")
+      .updateOne(
+        { _id: new mongodb.ObjectId(this._id) },
+        { $set: { cart: { items: [...updatedCartItems] } } }
+      );
+  }
+
   //   userId => string
   static findById(userId) {
     const db = getDb();
@@ -49,6 +132,55 @@ class User {
         .catch((err) => console.log(err))
     );
   }
+  //   this method here because "this" user executes an order
+  addOrder() {
+    //   ------------- ADDING RELATIONAL ORDER DATA -------------
+    const db = getDb();
+    return this.getCart()
+      .then((products) => {
+        // all the product information / quantity for the respective user
+        const order = {
+          // duplicate data - orders collection / user collection = availibitlity
+          // * but it is okay because a user will not often change its username ...
+          // * and also processed orders dont care for changed emails
+          items: products,
+          user: {
+            _id: new mongodb.ObjectId(this._id),
+            name: this.name,
+            email: this.email,
+          },
+          order_date: new Date(),
+        };
+
+        // the data for the order is already registered on this user
+        // * Either add the orders to the user or add the user to a order
+        // * because you have a lot of orders and embedding = chaos / big
+        // insertOne {items: []}
+        return db.collection("orders").insertOne(order);
+      })
+      .then((result) => {
+        //!  empty the cart locally
+        this.cart = { items: [] };
+        //! empty the cart in the db
+        return db
+          .collection("users")
+          .updateOne({ _id: this._id }, { $set: { cart: { items: [] } } });
+      });
+  }
+
+  getOrders() {
+    const db = getDb();
+    //   fetch orders from the given user
+    // * check nested properties by defining the path to them as a string
+    return db.collection("orders").find({ "user._id": this._id }).toArray();
+  }
+  //   getOrders() {
+  //     const db = getDb();
+  //     return db
+  //       .collection("orders")
+  //       .find({ "user._id": new mongodb.ObjectId(this._id) })
+  //       .toArray();
+  //   }
 }
 
 module.exports = User;
